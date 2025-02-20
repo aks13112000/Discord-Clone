1dfd092fdd979bf575b66c33b35180c0dcb79b42
Ajith ks <ajithksaji13112000@gmail.com>
2025-02-20 11:21:37 +0530

    Theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";  
 import { UserButton } from "@clerk/nextjs";
+  
 
 
 export default function Home() {
   return (
     <div>
- <UserButton/>
+ <UserButton
+  afterSignOutUrl="/"
+ />
+ <ModeToggle/>
     </div>
    
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -17,11 +19,24 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider afterSignOutUrl='/'>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={font.className}>
+        className={cn
+          (font.className,
+            "bg-white dark:bg-[#313338]"
+
+        )}>
+      <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
       
-        {children}
+      enableSystem={false}
+      storageKey="discord-theme"
+      >
+      {children}
+      </ThemeProvider>
+        
+        
       </body>
     </html>
     </ClerkProvider>
