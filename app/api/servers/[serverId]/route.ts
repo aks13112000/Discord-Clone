de0339ad788971392836b9dc5573db9ba60c06e0
Ajith ks <ajithksaji13112000@gmail.com>
2025-03-10 12:43:42 +0530

    delete&leave server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,52 +1,57 @@
-import { NextResponse } from "next/server";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
 
-export async function POST(req:Request){
+
+export async function DELETE(
+    req:Request, {params}:{params:{serverId:string}}
+){
     try{
         const profile=await currentProfile();
-        const {name,type}=await req.json();
-        const {searchParams}= new URL(req.url);
-        const serverId=searchParams.get("serverId");
 
         if(!profile){
             return new NextResponse("Unauthorized",{status:401});
         }
 
-        if(!serverId){
-            return new NextResponse("Server ID missing",{status:400});
-        }
+        const server=await db.server.delete({
+            where:{
+                id: params.serverId,
+                profileId: profile.id,
+            }
+          });
+        return NextResponse.json(server);
+    }
+    catch(error){
+        console.log("[SERVER_ID_DELETE]",error);
+        return new NextResponse("Internal Error",{status:500});
+    }
+}
 
-        if(name==="general"){
-            return new NextResponse("Channel name cannot be 'general'",{status:400});
+export async function PATCH(
+    req:Request, {params}:{params:{serverId:string}}
+){
+    try{
+        const profile=await currentProfile();
+        const {name,imageUrl}=await req.json();
+
+        if(!profile){
+            return new NextResponse("Unauthorized",{status:401});
         }
 
         const server=await db.server.update({
             where:{
-                id:serverId,
-                members:{
-                    some:{
-                        profileId:profile.id, role:{
-                            in:[MemberRole.ADMIN,MemberRole.MODERATOR]
-                        }
-                    }
-                }
+                id: params.serverId,
+                profileId: profile.id,
             },
             data:{
-                channels:{
-                    create:{
-                        profileId:profile.id,
-                        name,
-                        type,
-                    }
-                }
+                name,
+                imageUrl
             }
-    });
-    return NextResponse.json(server);
-}
+        });
+        return NextResponse.json(server);
+    }
     catch(error){
-        console.log("[CHANNELS_POST]",error);
+        console.log("[SERVER_ID_PATCH]",error);
         return new NextResponse("Internal Error",{status:500});
     }
 }
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -2,7 +2,7 @@ import { Server } from "http";
 import { create } from "zustand";
 
 export type ModalType = "createServer" | "invite" | "editServer" |
-"members"|"createChannel";
+"members"|"createChannel" |"leaveServer" | "deleteServer";
 
 
 interface ModalData {
