af1078944dc6519177acc3a1b41e211a7004a76f
Aks1579 <160128018+Aks1579@users.noreply.github.com>
2025-04-04 15:04:40 +0530

    Add ChatInput component and integrate into ChannelIdPage

--- app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { RedirectToSignIn } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
     params: {
@@ -34,7 +35,13 @@ const ChannelIdPage = async ({params}:ChannelIdPageProps) => {
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
         <ChatHeader name={channel.name} serverId={channel.serverId} type="channel"/>
+        <div className="flex-1">Futue Messages</div>
+        <ChatInput name={channel.name} type="channel" apiUrl="/api/socket/messages" query={{
+            channelId:channel.id,
+            serverId:channel.serverId,
+        }}/>
         </div>
+        
     );
     }
 
