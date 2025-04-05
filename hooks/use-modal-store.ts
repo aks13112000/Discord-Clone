88bbd32d4bcbe23a5731ab7e4337b78f47cf91bc
Aks1579 <160128018+Aks1579@users.noreply.github.com>
2025-04-05 14:40:52 +0530

    Add MessageFileModal component and integrate into modal provider; update modal store for file upload handling

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -3,13 +3,15 @@ import { Server } from "http";
 import { create } from "zustand";
 
 export type ModalType = "createServer" | "invite" | "editServer" |
-"members"|"createChannel" |"leaveServer" | "deleteServer"|"deleteChannel"|"editChannel"|"editMember"|"editRole"|"editProfile"|"editServerIcon"|"editServerBanner"|"editServerName"|"editChannelName"|"editChannelType"|"deleteMember"|"deleteRole"|"deleteChannel"|"deleteServerIcon"|"deleteServerBanner";
+"members"|"createChannel" |"leaveServer" | "deleteServer"|"deleteChannel"|"editChannel"|"editMember"|"editRole"|"editProfile"|"editServerIcon"|"editServerBanner"|"editServerName"|"editChannelName"|"editChannelType"|"deleteMember"|"deleteRole"|"deleteChannel"|"deleteServerIcon"|"deleteServerBanner"|"messageFile";
 
 
 interface ModalData {
     server?: Server;
     channel?:Channel;
     channelType?: ChannelType;
+    apiUrl?: string;
+    query?: Record<string,any>;
 }
 
 interface ModalStore {
