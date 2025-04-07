f4730ec757def5a0934e93dcdda29a92a8eb445a
Aks1579 <160128018+Aks1579@users.noreply.github.com>
2025-04-07 14:18:59 +0530

    Pending fix: Chat message component

--- app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 
 interface ChannelIdPageProps {
     params: {
@@ -35,7 +36,7 @@ const ChannelIdPage = async ({params}:ChannelIdPageProps) => {
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-screen">
         <ChatHeader name={channel.name} serverId={channel.serverId} type="channel"/>
-        <div className="flex-1">Futue Messages</div>
+        <ChatMessages member={member} name={channel.name} type="channel" apiUrl="/api/messages" socketUrl="/api/socket/messages" socketQuery={{channelId:channel.id,serverId:channel.serverId}} paramKey="channelId" paramValue={channel.id} chatId={channel.id}/>
         <ChatInput name={channel.name} type="channel" apiUrl="/api/socket/messages" query={{
             channelId:channel.id,
             serverId:channel.serverId,
