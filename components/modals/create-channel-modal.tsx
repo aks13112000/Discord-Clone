19846c1312cc503449867efcf86ea7c42cbdc26c
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-06 12:06:41 +0530

    Update create-channel-modal.tsx

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 import * as z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm,Controller} from "react-hook-form";
+import {useForm} from "react-hook-form";
 import qs from "query-string";
 
 
