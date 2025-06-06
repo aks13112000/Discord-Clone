78ef2990808a862c5800a5feb5a671fb55149b95
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-06 12:15:55 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,10 @@
 import {PrismaClient} from '@prisma/client';
 
 
-
+/* eslint-disable no-var */
 declare global{
     var prisma: PrismaClient|undefined;
 }
-
+/* eslint-disable no-var */
 export const db = globalThis.prisma||new PrismaClient();
 if(process.env.NODE_ENV !== 'production') globalThis.prisma = db;
