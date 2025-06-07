5b7c9150fd3a3597d1572f3e24870c53e9fda617
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-07 12:24:40 +0530

    Update page.tsx

--- app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
@@ -9,10 +9,10 @@ import { ChannelType } from "@prisma/client";
 import { MediaRoom } from "@/components/media-room";
 
 interface ChannelIdPageProps {
-    params: {
+    params: Promise<{
         serverId: string;
         channelId: string;
-    };
+    }>;
 }
 
 const ChannelIdPage = async ({params}:ChannelIdPageProps) => {
