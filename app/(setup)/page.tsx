ee1aa08e009aeab6ae41b755c0c27534f75d4674
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-10 18:56:55 +0530

    Update page.tsx

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,13 +2,17 @@ import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { InitialModal } from "@/components/modals/initial-modal";
+import { RedirectToSignIn } from "@clerk/nextjs";
 const SetupPage = async () => {
     const profile = await initialProfile();
+    if(!profile){
+    return RedirectToSignIn({});
+    }
     const server = await db.server.findFirst({
         where:{
             members:{
                 some:{
-                    profileId: profile?.id?
+                    profileId: profile.id
                 }
             }
         }
