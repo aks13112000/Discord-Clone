4f4ca5147f42f228c1410f60fbb17a79ce9b3787
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-10 19:24:40 +0530

    Update route.ts

--- app/api/servers/[serverId]/[invite-code]/route.ts
+++ app/api/servers/[serverId]/[invite-code]/route.ts
@@ -8,19 +8,20 @@ export async function PATCH(
     { params }: { params:Promise<{ serverId: string }> }
 ) {
    try{
+    const {serverId} = await params;
      const profile = await currentProfile();
 
      if (!profile) {
          return new NextResponse("Unauthorized", { status: 401 });
      }
 
-     if (!params.serverId) {
+     if (!serverId) {
          return new NextResponse("Server ID Missing", { status: 400 });
      }
 
      const server = await db.server.update({
         where: {
-            id: params.serverId,
+            id: serverId,
             profileId: profile.id,
         },
         data: {
