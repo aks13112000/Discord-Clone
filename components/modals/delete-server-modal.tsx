0286dcef9f618a71e22c41f433a93fa0704ee26e
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-13 11:25:16 +0530

    Update delete-server-modal.tsx

--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -57,7 +57,7 @@ export const DeleteServerModal = () => {
                     <DialogDescription className="text-center text-zinc-500">
                         Are you sure you want to do this? <br/> 
                         <span
-                        className="font-semibold text-indigo-500">{/* @ts-expect-error: expect null name */}{server?.name}</span>
+                        className="font-semibold text-indigo-500">{/* @ts-expect-error: expect null name */server?.name}</span>
                         will be permanently deleted.
                     </DialogDescription>
                 </DialogHeader>
