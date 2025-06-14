429f67f25b5d2fb8805d9d16762a0907c2f51b8e
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-14 12:57:35 +0530

    Update server-header.tsx

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -35,7 +35,9 @@ interface ServerHeaderProps {
                     </DropdownMenuItem>
                 )}
                 {isAdmin&&(
-                    <DropdownMenuItem onClick={()=>onOpen("editServer",{server})} className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                         // @ts-expect-error:expect-null server
+                        onClick={()=>onOpen("editServer",{server})} className="px-3 py-2 text-sm cursor-pointer">
                         Server Settings
                         <Settings className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
