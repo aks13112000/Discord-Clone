0ee9b094eb31fd7851546b04e93defcf55d2c4be
aks13112000 <161295118+aks13112000@users.noreply.github.com>
2025-06-14 12:53:11 +0530

    Update server-channel.tsx

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -78,6 +78,7 @@ export const ServerChannel = ({
                 <Trash
                 onClick={(e) => {
     onAction(e, "deleteChannel");
+                         // @ts-expect-error:expect-null server
     onOpen("deleteChannel", { server, channel });
   }}
                 className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"/>
